f6330ccb0b57b888fa1956f9e55c9ac09996f4bc
Heather Luttrell <possumdiva@heatherluttrell.com>
2020-10-24 10:09:46 -0400

    review work

--- js/app.js
+++ js/app.js
@@ -1,1 +1,17 @@
-console.log("hello world");
+const stringifyFormData = (fd) => {
+  const data = {};
+  for (let field of fd.keys()) {
+    data[field] = fd.get(field);
+  }
+  return JSON.stringify(data, null, 2);
+};
+
+const handleSubmit = (e) => {
+  e.preventDefault();
+  const data = new FormData(e.target);
+  const stringified = stringifyFormData(data);
+  console.log(stringified);
+};
+
+const form = document.getElementById("form");
+form.addEventListener("submit", handleSubmit);
